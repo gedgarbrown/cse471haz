ecc9648fda909cb362aa0669674e1d5e160885e6
GeorgeCavaco <georgecavaquinho@hotmail.com>
2020-06-06 16:43:09 -0600

    css and index, starting out

---
+++ index.php
@@ -0,0 +1,60 @@
+<?php
+	session_start();
+?>
+
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" type="text/css" href="haz.css">
+	
+	<title>HAZ Prototype CSE 471</title>
+</head>
+<body onload="loadIndex()">	
+	<div id="mySidenav" class="sidenav">
+	
+		
+
+	</div>	
+	<div id="main" class="main">
+		<div id="header" class="header">
+			<span class="center"> Hike Arizona</span>
+			<br>
+			<br>
+			
+		</div>			
+		<div>
+			<!-- Row 1 -->
+			<div class="row">	
+				<div class="col-3">&nbsp;
+				</div>
+				<div class="col-6 center">
+					
+				
+				</div>
+				<div class="col-3">&nbsp;
+				</div>
+			</div>
+			<!-- Row 2 -->
+			
+			<div class="row">	
+				<div class="col-3">&nbsp;
+				</div>
+				<div class="col-6">
+					
+				</div>
+				<div class="col-3">&nbsp;
+				</div>
+			</div>
+			
+			
+		<div>
+		
+		
+	</div>
+	
+	
+
+</body>
+</html>
