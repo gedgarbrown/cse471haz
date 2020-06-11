7fe9179ba1a765431f8b0e2bbef56ea2a2f806eb
GeorgeCavaco <georgecavaquinho@hotmail.com>
2020-06-11 13:34:56 -0600

    New Guides/pictures

---
+++ guides/auger_trail/auger_trail.php
@@ -0,0 +1,71 @@
+<?php
+	session_start();
+?>
+
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" type="text/css" href="../../haz.css">
+	
+	<title>HAZ Prototype CSE 471</title>
+</head>
+<body onload="loadIndex()">	
+	<div id="mySidenav" class="sidenav">
+	
+		
+
+	</div>	
+	<div id="main" class="main">
+		<div id="header" class="header">
+			<div class="center header"> Hike Arizona</div>
+			&nbsp;
+			<a href="../../index.php"><img src="../../images/home.jpg" width = "25" height ="20"></a>
+			&nbsp;
+			<a href="../../find.php"><img src="../../images/find.jpg" width = "25" height ="20"></a>
+			&nbsp;
+			<a href="../../plan.php"><img src="../../images/plan.jpg" width = "25" height ="20"></a>
+			&nbsp;
+			<a href="../../connect.php"><img src="../../images/connect.jpg" width = "25" height ="20"></a>
+			&nbsp;
+			<a href="../../login.php"><img src="../../images/login.jpg" width = "25" height ="20"></a>
+			&nbsp;
+			
+		</div>	
+		<hr>
+		<div class="center bold">Auger Trail</div>
+		<div>
+			<!-- Row 1 -->
+			<div class="row">	
+				<div class="col-3">&nbsp;
+				</div>
+				<div class="col-6 center">
+					
+				
+				</div>
+				<div class="col-3">&nbsp;
+				</div>
+			</div>
+			<!-- Row 2 -->
+			
+			<div class="row">	
+				<div class="col-3">&nbsp;
+				</div>
+				<div class="col-6">
+					
+				</div>
+				<div class="col-3">&nbsp;
+				</div>
+			</div>
+			
+			
+		<div>
+		
+		
+	</div>
+	
+	
+
+</body>
+</html>
--- newguides.php
+++ newguides.php
@@ -38,31 +38,202 @@
 		<div>
 			<!-- Row 1 -->
 			<div class="row">	
-				<div class="col-3">&nbsp;
-				</div>
-				<div class="col-6 center">
-					
+				
+				<div class="col-1 center">
+				05-27	
 				
 				</div>
-				<div class="col-3">&nbsp;
-				</div>
-			</div>
-			<!-- Row 2 -->
-			
-			<div class="row">	
-				<div class="col-3">&nbsp;
-				</div>
-				<div class="col-6">
+				<div class="col-8">
+				<a href="guides/auger_trail/auger_trail.php">Auger Trail #609</a>
+				<br>
 					
+					<img src="guides/auger_trail/auger_trail_01.jpg" height = "75px" >
+					
+				&nbsp
+					
+						<img src="guides/auger_trail/auger_trail_02.jpg" height = "75px" >
+					
+				&nbsp
+					
+						<img src="guides/auger_trail/auger_trail_03.jpg" height = "75px"  >
+						
+				&nbsp
+				<a href="guides/auger_trail/auger_trail_pictures.php">
+					more pictures
+				</a>
+				
 				</div>
-				<div class="col-3">&nbsp;
+				<div class="col-1 center">
+				AZ	
+				
+				</div>
+				<div class="col-1 center">
+				friendofThunder	
+				
+				</div>
+				<div class="col-1">&nbsp;
+				</div>
+			</div>
+	<!-- Row 2 -->
+			<div class="row">	
+				
+				<div class="col-1 center">
+				05-25	
+				
+				</div>
+				<div class="col-8">
+				<a href="guides/mtgarfield/mtgarfield.php">Mt. Garfield</a>
+				<br>
+					
+					<img src="guides/mtgarfield/mtgarfield_01.jpg" height = "75px" >
+					
+				&nbsp
+					
+						<img src="guides/mtgarfield/mtgarfield_02.jpg" height = "75px" >
+					
+				&nbsp
+					
+						<img src="guides/mtgarfield/mtgarfield_03.jpg" height = "75px"  >
+						
+				&nbsp
+				<a href="guides/mtgarfield/mtgarfield.php">
+					more pictures
+				</a>
+				
+				</div>
+				<div class="col-1 center">
+				CO	
+				
+				</div>
+				<div class="col-1 center">
+				 PaleoRob
+				
+				</div>
+				<div class="col-1">&nbsp;
 				</div>
 			</div>
 			
-			
-		<div>
-		
-		
+		<!-- Row 3 -->
+			<div class="row">	
+				
+				<div class="col-1 center">
+				05-25	
+				
+				</div>
+				<div class="col-8">
+				<a href="guides/klunker_trail/klunker_trail.php">Klunker Trail - Miramonte Rim to Holy Cross  </a>
+				<br>
+					
+					<img src="guides/klunker_trail/klunker_trail_01.jpg" height = "75px" >
+					
+				&nbsp
+					
+						<img src="guides/klunker_trail/klunker_trail_02.jpg" height = "75px" >
+					
+				&nbsp
+					
+						<img src="guides/klunker_trail/klunker_trail_03.jpg" height = "75px"  >
+						
+				&nbsp
+				<a href="guides/klunker_trail/klunker_trail.php">
+					more pictures
+				</a>
+				
+				</div>
+				<div class="col-1 center">
+				CO	
+				
+				</div>
+				<div class="col-1 center">
+				 PaleoRob
+				
+				</div>
+				<div class="col-1">&nbsp;
+				</div>
+			</div>	
+		<!-- Row 4 -->
+			<div class="row">	
+				
+				<div class="col-1 center">
+				05-23	
+				
+				</div>
+				<div class="col-8">
+				<a href="guides/laboca/laboca.php">La Boca</a>
+				<br>
+					
+					<img src="guides/laboca/laboca_01.jpg" height = "75px" >
+					&nbsp; &nbsp;
+				
+				<a href="guides/auger_trail/auger_trail_pictures.php">
+					more pictures
+				</a>
+				
+				</div>
+				<div class="col-1 center">
+				AZ	
+				
+				</div>
+				<div class="col-1 center">
+				friendofThunder	
+				
+				</div>
+				<div class="col-1">&nbsp;
+				</div>
+			</div>
+		<!-- Row 5 -->
+			<div class="row">	
+				
+				<div class="col-1 center">
+				05-25	
+				
+				</div>
+				<div class="col-8">
+				<a href="guides/klunker_trailhead/klunker_trailhead.php">Klunker Trail - Trailhead to Miramonte Rim   </a>
+				<br>
+					
+					<img src="guides/klunker_trailhead/klunker_trailhead_01.jpg" height = "75px" >
+					
+				&nbsp
+					
+						<img src="guides/klunker_trailhead/klunker_trailhead_02.jpg" height = "75px" >
+					
+				&nbsp
+					
+						<img src="guides/klunker_trailhead/klunker_trailhead_03.jpg" height = "75px"  >
+						
+				&nbsp
+				<a href="guides/klunker_trail/klunker_trailhead.php">
+					more pictures
+				</a>
+				
+				</div>
+				<div class="col-1 center">
+				CO	
+				
+				</div>
+				<div class="col-1 center">
+				 PaleoRob
+				
+				</div>
+				<div class="col-1">&nbsp;
+				</div>
+			</div>
+		<!-- Row 6 -->
+			<div class="row">
+				<div class="col-12">&nbsp;
+				</div>
+			</div>
+		<!-- Row 7 -->
+			<div class="row">
+				<div class="col-12 center"><a href="more.php">... Load More ...</a>
+				</div>
+			</div>
+		<!-- Row 8 -->
+			<div class="row">
+				<div class="col-12">&nbsp;
+				</div>
+			</div>
 	</div>
 	
 	
